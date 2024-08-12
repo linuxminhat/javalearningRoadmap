2cf8b32a65107c5e5403057c4f6124da35685280
linuxminhat <dangnhatminhdhbk@gmail.com>
2024-08-12 11:40:29 +0700

    commit collection

---
+++ Collection/list1.java
@@ -0,0 +1,13 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class list1 {
+	public static void main(String[] args) {
+		List<String> word = new ArrayList<>(Arrays.asList("Dog"));
+		System.out.println(word);
+	}
+
+}
